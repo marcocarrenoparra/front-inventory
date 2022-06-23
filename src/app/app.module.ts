10c6e30f091d790e9d10b01b20172dd2f66a5815
acercade <marco.carreno@acercade.cl>
2022-06-22 22:05:08 -0400

    primer inentario ront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
